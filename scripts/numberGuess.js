cb83acdcfcb1615f8a283a6d8668c815618a2400
Adam Pizzo <50724722+adampizzo@users.noreply.github.com>
2023-05-01 14:16:36 -0400

    Wrote very basic number guessing logic.

--- scripts/numberGuess.js
+++ scripts/numberGuess.js
@@ -1,12 +1,23 @@
 let numToGuess = Math.floor(Math.random() * 20);
 console.log(`Number to Guess: ${numToGuess}.`);
 
-
 const playerGuess = () => {
-    console.log(playerGuessInput.value);
-    
+    const playerGuess = parseInt(playerGuessInput.value);
+    evaluateGuess(playerGuess);
+};
+
+const evaluateGuess = (guess) => {
+    guessStatus.removeAttribute("hidden");
+    if (guess === numToGuess) {
+        guessStatus.innerText = "You got it!";
+    } else if (guess < numToGuess) {
+        guessStatus.innerText = "No, try a higher number.";
+    } else {
+        guessStatus.innerText = "No, try a lower number.";
+    }
 };
 
 const guessBtn = document.getElementById("guessBtn");
+const guessStatus = document.getElementById("guessStatus");
 
 guessBtn.addEventListener("click", playerGuess);
